fdef16f71c192f7ca30cb3743bf9ce86b5e45451
Joshua French <osakenoyojimbo@gmail.com>
2008-06-05 23:43:25 -0500

    Flipping more of the generators over to spec, 5 down, 3 to go

---
+++ generators/hobo_model_controller_spec/hobo_model_controller_spec_generator.rb
@@ -0,0 +1,55 @@
+# Another piece that I'm not even sure I need, so we'll comment it out for now
+# require 'rails_generator/generators/components/controller/controller_generator'
+
+class HoboModelControllerGenerator < Rails::Generator::NamedBase
+
+  def initialize(args, options)
+    args[0] = args[0].pluralize
+    super(args, options)
+  end
+
+  def manifest
+    record do |m|
+      # Check for class naming collisions.
+      m.class_collisions class_path, "#{class_name}Controller", "#{class_name}ControllerTest", "#{class_name}Helper"
+
+      # Controller, helper, views, and spec directories.
+      m.directory File.join('app/controllers', class_path)
+      m.directory File.join('app/helpers', class_path)
+      m.directory File.join('app/views', class_path, file_name)
+      m.directory File.join('spec/controllers', class_path)
+      m.directory File.join('spec/helpers', class_path)
+      m.directory File.join('spec/views', class_path, file_name)
+      
+      if Rails::VERSION::STRING < "2.0.0"
+        @default_file_extension = "rhtml"
+      else
+        @default_file_extension = "html.erb"
+      end
+
+      # Controller spec, class, and helper.
+      m.template 'controller_spec.rb',
+        File.join('spec/controllers', class_path, "#{file_name}_controller_spec.rb")
+
+      m.template 'helper_spec.rb',
+        File.join('spec/helpers', class_path, "#{file_name}_helper_spec.rb")
+
+      m.template 'controller:controller.rb',
+        File.join('app/controllers', class_path, "#{file_name}_controller.rb")
+
+      m.template 'controller:helper.rb',
+        File.join('app/helpers', class_path, "#{file_name}_helper.rb")
+
+      # Spec and view template for each action.
+      actions.each do |action|
+        m.template 'view_spec.rb',
+          File.join('spec/views', class_path, file_name, "#{action}.#{@default_file_extension}_spec.rb"),
+          :assigns => { :action => action, :model => file_name }
+        path = File.join('app/views', class_path, file_name, "#{action}.#{@default_file_extension}")
+        m.template "controller:view.#{@default_file_extension}",
+          path,
+          :assigns => { :action => action, :path => path }
+      end
+    end
+  end
+end
--- generators/hobo_user_model_spec/hobo_user_model_spec_generator.rb
+++ generators/hobo_user_model_spec/hobo_user_model_spec_generator.rb
@@ -1,4 +1,4 @@
-class HoboModelGenerator < Rails::Generator::NamedBase
+class HoboUserModelGenerator < Rails::Generator::NamedBase
 
   def manifest
     record do |m|
@@ -7,30 +7,19 @@ class HoboModelGenerator < Rails::Generator::NamedBase
 
       # Model, test, and fixture directories.
       m.directory File.join('app/models', class_path)
-      m.directory File.join('test/unit', class_path)
-      m.directory File.join('test/fixtures', class_path)
       m.directory File.join('spec/models', class_path)
       m.directory File.join('spec/fixtures', class_path)
 
-
       # Model class, unit test, and fixtures.
       m.template 'model.rb',      File.join('app/models', class_path, "#{file_name}.rb")
-      m.template 'unit_test.rb',  File.join('test/unit', class_path, "#{file_name}_test.rb")
-      m.template 'fixtures.yml',  File.join('test/fixtures', class_path, "#{table_name}.yml")
       m.template 'model:fixtures.yml',  File.join('spec/fixtures', class_path, "#{table_name}.yml")
       m.template 'model_spec.rb',       File.join('spec/models', class_path, "#{file_name}_spec.rb")
-
-      unless options[:skip_migration]
-        m.migration_template 'model:migration.rb', 'db/migrate', :assigns => {
-          :migration_name => "Create#{class_name.pluralize.gsub(/::/, '')}"
-        }, :migration_file_name => "create_#{file_path.gsub(/\//, '_').pluralize}"
-      end
     end
   end
 
   protected
     def banner
-      "Usage: #{$0} #{spec.name} ModelName [field:type, field:type]"
+      "Usage: #{$0} #{spec.name} ModelName"
     end
 
 end
